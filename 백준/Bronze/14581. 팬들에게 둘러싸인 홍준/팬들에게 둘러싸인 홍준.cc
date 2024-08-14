afc295d6823f8cfdd34248d7f75c9e41c947c6dd
Haewon Shon <53929798+HaewonShon@users.noreply.github.com>
2024-08-14 15:14:20 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    
+    string s;
+    cin>>s;
+
+    cout<<":fan::fan::fan:\n";
+    cout<<":fan::"<<s<<"::fan:\n";
+    cout<<":fan::fan::fan:\n";
+}
