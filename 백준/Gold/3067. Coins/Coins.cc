597d2b295ad939f9b0f5cca145402a88832f67d7
Haewon Shon <53929798+HaewonShon@users.noreply.github.com>
2024-11-11 01:47:35 +0900

    [Gold V] Title: Coins, Time: 328 ms, Memory: 2840 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[21][10001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n, m;
+        cin>>n;
+
+        vector<int> coins(n+1);
+        for(int i = 1; i <= n; ++i) cin>>coins[i];
+
+        cin>>m;
+
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+
+        for(int i = 1; i <= n; ++i)
+        {
+            memcpy(dp[i], dp[i-1], sizeof(dp[i]));
+
+            int coin = coins[i];
+            while(coin <= m)
+            {
+                for(int j = coin; j <= m; ++j)
+                {
+                    dp[i][j] += dp[i-1][j-coin];
+                }
+
+                coin += coins[i];
+            }
+        }
+
+        cout<<dp[n][m]<<"\n";
+    }
+}
