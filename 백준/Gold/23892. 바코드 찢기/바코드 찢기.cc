82406e41c37ea8887170bd71a41cd5ca00582f26
Haewon Shon <53929798+HaewonShon@users.noreply.github.com>
2025-08-19 23:36:43 +0900

    [Gold II] Title: 바코드 찢기, Time: 0 ms, Memory: 2532 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Gold/23892. 바코드 찢기/바코드 찢기.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long GetValue(const string& s, long long repeat)
+{
+    long long res = 0;
+    for(int i = 0; i < s.length() - 1; ++i)
+    {
+        if(s[i] == '(' && s[i+1] == ')') ++res;
+    }
+    res *= repeat;
+
+    if(s.front() == ')' && s.back() == '(') res += repeat - 1;
+
+    return res;
+}
+
+int main()
+{ 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n, k;
+    cin>>n>>k;
+
+    string s1, s2;
+    long long r1, r2;
+    cin>>s1>>r1>>s2>>r2;
+
+    long long initVal = GetValue(s1, r1);
+    long long drinkVal = GetValue(s2, r2);
+
+    if(initVal < k)
+    {
+        cout<<0;
+        return 0;
+    }
+
+    if(drinkVal >= k)
+    {
+        cout<<n;
+        return 0;
+    }
+
+    long long possibleDrinkCount = (initVal - k + 1) / (k - drinkVal);
+    cout<<min(possibleDrinkCount, n);
+}
