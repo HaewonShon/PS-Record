9b6307e85b65f2f551441995c717df331e7c4abc
Haewon Shon <53929798+HaewonShon@users.noreply.github.com>
2025-08-20 17:06:11 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 356 ms, Memory: 9696 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Step
+{
+    int r, c, k, turn;
+};
+
+int visited[1000][1000]; // 방문 시점 소모 k횟수
+
+int main()
+{ 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    memset(visited, 0x7f, sizeof(visited));
+
+    int n, m, k;
+    cin>>n>>m>>k;
+
+    vector<string> board(n);
+    for(int i = 0; i < n; ++i) cin>>board[i];
+
+    queue<Step> q;
+    q.push({0, 0, 0, 1});
+    visited[0][0] = 0;
+
+    int xOffset[4] = {-1, 1, 0, 0};
+    int yOffset[4] = {0, 0, -1 ,1};
+
+    while(!q.empty())
+    {
+        Step s = q.front();
+        q.pop();
+
+        if(s.r == n-1 && s.c == m-1)
+        {
+            cout<<s.turn;
+            return 0;
+        }
+
+        for(int i = 0; i < 4; ++i)
+        {
+            int r = s.r, c = s.c;
+            r += yOffset[i];
+            c += xOffset[i];
+
+            if(r >= 0 && r < n && c >= 0 && c < m)
+            {
+                if(board[r][c] == '0' && visited[r][c] > s.k) // 그냥 이동
+                {
+                    visited[r][c] = s.k;
+                    q.push({r, c, s.k, s.turn + 1});
+                }
+                else if(board[r][c] == '1' && s.k < k && visited[r][c] > s.k + 1) // 벽 부수고 이동
+                {
+                    if(s.turn % 2 == 1) // 낮 - 바로 이동
+                    {
+                        visited[r][c] = s.k + 1;
+                        q.push({r, c, s.k + 1, s.turn + 1});
+                    }
+                    else // 밤 - 한 턴 대기
+                    {
+                        q.push({s.r, s.c, s.k, s.turn + 1});
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<-1;
+}
